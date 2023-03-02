1431f28198aa134a23fdaecc1a7429d5a843d8bc
Jaipharma <jaipharma@hotmail.com>
2023-03-02 18:22:09 +0700

    add back link full styles page

--- pages/css/full-styles.js
+++ pages/css/full-styles.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout2, { siteTitle } from '../../components/layout2';
 import utilStyles from '../../styles/utils.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -14,6 +15,10 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <br></br>
+        <h2>
+        <Link href="/css">← Back</Link>
+      </h2>
       </section>
     </Layout2>
   );
